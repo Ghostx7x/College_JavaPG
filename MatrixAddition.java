4bf7c522b09de554ce7d9aefbc1795b5cc9122b1
Ghostx7x <112379929+Ghostx7x@users.noreply.github.com>
2024-01-16 06:07:14 +0530

    Update MatrixAddition.java

--- MatrixAddition.java
+++ MatrixAddition.java
@@ -13,17 +13,19 @@ public class MatrixAddition
         int[][] matrix1 = new int[M][N];
         int[][] matrix2 = new int[M][N];
         int[][] sum = new int[M][N];
+        Scanner input = new Scanner(System.in);
+        
         System.out.println("Enter elements of Matrix 1: ");
-        inputMatrix(matrix1);
+        inputMatrix(matrix1, input);
         System.out.println("Enter elements of Matrix 2: ");
-        inputMatrix(matrix2);
+        inputMatrix(matrix2, input);
         addMatrices(matrix1,matrix2,sum);
         displayMatrix(sum);
+        input.close();
     }
 
-    public static void inputMatrix(int[][] matrix)
+    public static void inputMatrix(int[][] matrix,Scanner input)
     {
-        Scanner input = new Scanner(System.in);
         for(int i = 0;i<matrix.length;i++)
         {
             for(int j = 0;j<matrix[0].length;j++)
@@ -32,7 +34,6 @@ public class MatrixAddition
                 matrix[i][j] = input.nextInt();
             }
         }
-        input.close();
     }
 
     public static void addMatrices(int[][] matrix1,int[][] matrix2,int[][] sum)
